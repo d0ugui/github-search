8a3c705807bb246a56c8a60a4254b5e56cee0589
Douglas Oliveira <douglaspo_97@outlook.com>
2022-02-01 17:26:23 -0300

    fix: add axios and create instance

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { FiSearch } from 'react-icons/fi';
-import Button from '../../components/Button';
+import React, { useState } from 'react'
+import api from '../../services/api';
 
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
+import { FiSearch } from 'react-icons/fi';
 import { Container, Content } from './styles';
 
 export const Home: React.FC = () => {
+  const [user, setUser] = useState<string>('');
+
+  async function handleSearch() {
+    const res = await api.get(user);
+    console.log(res.data);
+  }
+
   return (
     <Container>
       <h1>Search Devs!</h1>
       <Content>
-        <Input placeholder="Digite o nome do usuário"/>
-        <Button>
+        <Input 
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="Digite o nome do usuário"
+        />
+        <Button onClick={handleSearch}>
           <FiSearch />
           Buscar
         </Button>
